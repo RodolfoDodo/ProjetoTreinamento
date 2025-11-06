6499193a26fa266c6b7da4d429e08fa60b0fab10
RodolfoDodo <farleyrodolfo@gmail.com>
2025-11-06 14:53:51 -0300

    Configura Selenium para rodar em modo headless no GitHub Actions

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -1,12 +1,14 @@
 package steps;
 
 import io.cucumber.java.pt.Dado;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,6 +23,10 @@ public class StepDefinitions {
     public void tirarScreenshot(String nomeArquivo) {
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
+            File screenshotsDir = new File("screenshots");
+            if (!screenshotsDir.exists()) {
+                screenshotsDir.mkdirs();
+            }
             FileUtils.copyFile(screenshot, new File("screenshots/" + nomeArquivo + ".png"));
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,7 +35,24 @@ public class StepDefinitions {
 
     @Dado("que acesso à página")
     public void que_acesso_à_página() {
-        driver = new ChromeDriver();
+        // Configura o WebDriverManager para gerenciar o ChromeDriver
+        WebDriverManager.chromedriver().setup();
+        
+        // Configura opções do Chrome
+        ChromeOptions options = new ChromeOptions();
+        
+        // Verifica se está rodando em CI (GitHub Actions) ou localmente
+        String ci = System.getenv("CI");
+        if (ci != null && ci.equals("true")) {
+            // Modo headless para CI (sem interface gráfica)
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+        }
+        options.addArguments("--window-size=1920,1080");
+        
+        driver = new ChromeDriver(options);
         driver.get("https://mediumseagreen-sheep-627446.hostingersite.com/");
         tirarScreenshot("acesso");
 
